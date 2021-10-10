e670284b7cf96d99e404c40b13a37c4e50b9b058
unknown <amulyamadupoju14@gmail.com>
2021-10-10 14:08:16 +0530

    example

--- routes/Scroll_To_Top_Button.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('Scroll_To_Top_Button', { title: 'Express' });
-});
-
-module.exports = router;
--- routes/Sticky_Navbar.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('Sticky_Navbar', { title: 'Express' });
-});
-
-module.exports = router;
